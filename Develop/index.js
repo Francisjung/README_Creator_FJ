dd92f1655f178ece8d4022330dea582ebd5b2a25
Francis Jung <francisjung17@gmail.com>
2023-05-03 06:18:54 -0500

    added changes to js

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const gm = require('generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -11,23 +12,28 @@ const questions = [
     'Feature #2',
     'Feature #3',
     'Feature #4',
+    'Which License do you need for this project?'
 ];
 let answers=[];
-for(let i=0;i<7;i++){
-    inquirer.prompt(
-        {
-        type: 'input',
-        message: questions[i],
-        name:'question'+[i]
-        }
-).then((response) =>{
-    answers.push(response);
-});
+
+function inquire(){
+    for(let i=0;i<8;i++){
+        inquirer.prompt(
+            {
+            type: 'input',
+            message: questions[i],
+            name:'question'+[i]
+            }
+    ).then((response) =>{
+        answers.push(response);
+    });
+    }
 }
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    const template = `# ${data[0]}
+    const template = 
+`# ${data[0]}
 
 ## Description
 
@@ -43,27 +49,7 @@ function writeToFile(fileName, data) {
 
 ## License
 
-MIT License
-
-Copyright (c) [2023] [Francis Jung]
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+${data[7]}
 
 ## Features
 
@@ -71,10 +57,16 @@ SOFTWARE.
 - ${data[4]}.
 - ${data[5]}.
 - ${data[6]}`;
+fs.writeFile('README.MD', template, (err) =>
+  err ? console.error(err) : console.log('Success!')
+);
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquire();
+
+}
 
 // Function call to initialize app
 init();
